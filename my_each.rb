577a1196c5aa2bd7baf0136c31d9a69ee3ceca00
Jess Fischbach <robotspacefish@gmail.com>
2019-11-06 14:13:12 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(args) # put argument(s) here
   # code here
+  count = 0
+  while count < args.length
+    yield(args[count]) if block_given?
+    count += 1
+  end
+  args
 end
