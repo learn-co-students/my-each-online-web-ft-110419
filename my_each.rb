bfa53a2dddedecca914a1401533964adae1929a2
Ivana <ivana_pile@yahoo.com>
2019-11-07 15:24:12 -0500

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each 
+  collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  puts i
 end
+end
+
+def my_each(words) do |word|
+      puts word
+    end
+  end
