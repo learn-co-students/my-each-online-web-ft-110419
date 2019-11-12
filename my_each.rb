8a82a0398de4398ae45c78c8b9e740d11b748f1c
Taishawn King <taishawnking1@yahoo.com>
2019-11-12 21:38:26 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+require "pry"
+def my_each(array)
+greeting = 0
+while greeting < array.size
+phrase =array[greeting]
+greeting += 1
+yield phrase
 end
+return array
+end 
